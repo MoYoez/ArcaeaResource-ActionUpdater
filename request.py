96a479fdabdc9329af31823ceeb4706f3149a13a
MoYoez <i@himoyo.cn>
2023-07-19 18:28:02 +0800

    fixed

--- request.py
+++ request.py
@@ -8,13 +8,13 @@ url = "https://webapi.lowiro.com/webapi/serve/static/bin/arcaea/apk"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 "
-                  "Safari/537.36",
+    "Safari/537.36",
     "Referer": "https://arcaea.lowiro.com",
     "Set-Cookie": "ctrcode=CN; domain=lowiro.com",
     "Access-Control-Allow-Origin": "https://arcaea.lowiro.com",
 }
 
-if sys.argv[1] is not None:
+if len(sys.argv) > 1:
     get_url = sys.argv[1]
     get_version = sys.argv[1]
 else:
